5c6ce5fa2ceeac777492f8a88cbac3abd406484d
Chuck Greb <charles.greb@gmail.com>
2013-05-03 11:20:05 -0400

    Adds button click listener.

--- HelloTDDiOS/ViewController.m
+++ HelloTDDiOS/ViewController.m
@@ -7,23 +7,30 @@
 //
 
 #import "ViewController.h"
+#import "DDLog.h"
 
 @interface ViewController ()
+
+- (IBAction)onButtonClick:(id)sender;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad
-{
+static const int ddLogLevel = LOG_LEVEL_VERBOSE;
+
+- (void)viewDidLoad {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning
-{
+- (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)onButtonClick:(id)sender {
+    DDLogVerbose(@"onButtonClick");
 }
 
 @end
